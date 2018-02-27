3adb26fec8bd89b486cf076d40d8992e70725fb4
Noura Kaddoura <nourakaddoura@gmail.com>
2018-02-27 15:25:19 -0800

    animate.css added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
         session[:user_id] = @user.id
         redirect_to user_path(@user)
       else
-        flash[:warning] = "Oops, Somethings not correct. Try again" 
+        flash[:warning] = "Oops, not correct. Try again" 
         redirect_to new_session_path
       end
   end
